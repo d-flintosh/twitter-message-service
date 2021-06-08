25f439b319e5b030c5667cfcbb5b1e57c6d09718
Drew Flintosh <>
2021-06-08 15:17:03 -0500

    add cloud build

--- project-setup.sh
+++ project-setup.sh
@@ -10,3 +10,7 @@ enable_service "secretmanager.googleapis.com"
 #gcloud secrets create twitter-automation-001
 #gcloud secrets versions add twitter-automation-001 --data-file="path-to-secrets"
 
+#gcloud beta builds triggers create cloud-source-repositories \
+#    --repo=d-flintosh/twitter-message-service \
+#    --branch-pattern="^master$" \
+#    --build-config=./cloud-build.yaml
